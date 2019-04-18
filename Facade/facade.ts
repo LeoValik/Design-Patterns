12fd0067b93d6607da6d4bcf9115b05d59ddd0a5
LeoValik <troval12989@gmail.com>
2019-04-18 11:52:46 +0300

    Without Facade

---
+++ Facade/facade.ts
@@ -0,0 +1,68 @@
+class Table {
+
+    chooseTable() {
+      console.log('Choose table into cafe');
+    }
+  
+    sitOnTheTable() {
+      console.log('Sit down to the table');
+    }
+  
+  }
+  
+  class Menu {
+  
+    askForMenu() {
+      console.log('Ask for menu');
+    }
+  
+    openMenu() {
+      console.log('Open the menu');
+    }
+  
+    chooseDishes() {
+      console.log('Choose dishes from menu');
+    }
+  
+  }
+  
+  class MakeOrder {
+  
+    askWaiterToCome() {
+      console.log('Ask waiter come to your table');
+    }
+  
+    makeAnOrder() {
+      console.log('Make the order from menu');
+    }
+  
+    closeMenu() {
+      console.log('Close the menu');
+    }
+  
+    giveMenuToWaiter() {
+      console.log('Give menu to the waiter');
+    }
+  
+  }
+  
+  class Pay {
+  
+    askWaiterForCheck() {
+      console.log('Ask waiter for check of your dishes');
+    }
+  
+    putMoneyToCheck() {
+      console.log('Put money for the order');
+    }
+  
+    giveMoneyWithCheckToWaiter() {
+      console.log('Give money with check to the waiter');
+    }
+  
+    getUpFromTable() {
+      console.log('Get up from the table');
+    }
+  
+  }
+  
